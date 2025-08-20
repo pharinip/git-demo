95b42ac6500747f0b51e0038a38b7002fff764a5
pharinip <harinip000@gmail.com>
2025-08-20 13:25:23 +0530

    Added for loop in Test class

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
     public void sampleCode(){
         System.out.println("Hi Welcome");
+        for(int i=0;i<5;i++){
+            System.out.println("this is a sample code");
+        }
     }
 
 }
